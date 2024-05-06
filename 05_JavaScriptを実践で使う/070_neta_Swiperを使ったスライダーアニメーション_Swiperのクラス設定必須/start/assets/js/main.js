c0dbc8511d1be3dc15c8d469ebdaf3bb04d2d3d6
Nobuyuki Takahiro <vivlio@kumihan.com>
2024-05-06 12:24:00 +0900

    slider lesson once more

--- 05_JavaScriptを実践で使う/070_neta_Swiperを使ったスライダーアニメーション_Swiperのクラス設定必須/start/assets/js/main.js
+++ 05_JavaScriptを実践で使う/070_neta_Swiperを使ったスライダーアニメーション_Swiperのクラス設定必須/start/assets/js/main.js
@@ -1,12 +1,29 @@
-const swiper = new Swiper('.swiper', {
-  // effect: 'fade',
-  effect: 'coverflow',
-  loop: true,
-  speed: 3000, // ループの時間
-  autoplay: {
-    delay: 6000, // 途切れなくループ
-  },  
+document.addEventListener('DOMContentLoaded', function() {
+  const hs = new HeroSlider('.swiper');
 });
+
+class HeroSlider {
+  constructor(elm) {
+    this.elm = elm;
+    this.swiper = new Swiper('.swiper', {
+      // effect: 'fade',
+      effect: 'coverflow',
+      loop: true,
+      speed: 1000, // ループの時間
+      autoplay: {
+        delay: 5000, // 途切れなくループ
+      },
+      centeredSlides: true,
+      slidePerView: 2,
+      breakposts: {
+        1024: {
+          slidePerView: 2
+        }
+      }
+    });
+  }
+};
+
 
 // direction: 'vertical',
 // loop: true, // ループ有効
