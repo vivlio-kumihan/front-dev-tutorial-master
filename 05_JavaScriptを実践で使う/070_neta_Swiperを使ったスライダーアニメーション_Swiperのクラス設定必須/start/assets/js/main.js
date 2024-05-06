248f3735c4b8dc98877d615234788e9c402c0ce5
Nobuyuki Takahiro <vivlio@kumihan.com>
2024-05-06 17:20:16 +0900

    swiper local rule

--- 05_JavaScriptを実践で使う/070_neta_Swiperを使ったスライダーアニメーション_Swiperのクラス設定必須/start/assets/js/main.js
+++ 05_JavaScriptを実践で使う/070_neta_Swiperを使ったスライダーアニメーション_Swiperのクラス設定必須/start/assets/js/main.js
@@ -1,31 +1,56 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const hs = new HeroSlider('.swiper');
+const swiper = new Swiper('.swiper', {
+  effect: 'coverflow',
+  loop: true,
+  loopAdditionalSlides: 1, 
+  speed: 1000, //=> 1秒のアニメーション
+  autoplay: {
+    delay: 2000, //=> 2秒間静止
+  },
 });
 
-class HeroSlider {
-  constructor(elm) {
-    this.elm = elm;
-    this.swiper = new Swiper('.swiper', {
-      // effect: 'fade',
-      effect: 'coverflow',
-      loop: true,
-      speed: 1000, // ループの時間
-      autoplay: {
-        delay: 5000, // 途切れなくループ
-      },
-      centeredSlides: true,
-      slidePerView: 2,
-      breakposts: {
-        1024: {
-          slidePerView: 2
-        }
-      }
-    });
-  }
-};
+// swiper.on('reachEnd', function () {
+//   swiper.slides.eq(0).clone().appendTo('.swiper-wrapper');
+//   swiper.update();
+// });
+
+// swiper.on('reachEnd', function () {
+//   const originalSlides = swiper.slides;
+//   const clonedSlide = originalSlides.eq(0).clone();
+//   swiper.appendSlide(clonedSlide);
+//   swiper.removeSlide(0);
+// });
 
 
-// direction: 'vertical',
-// loop: true, // ループ有効
-// slidesPerView: 3, // 一度に表示する枚数
-// allowTouchMove: false, // スワイプ無効
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   const hs = new HeroSlider('.swiper');
+// });
+
+// class HeroSlider {
+//   constructor(elm) {
+//     this.elm = elm;
+//     this.swiper = new Swiper('.swiper', {
+//       // effect: 'fade',
+//       effect: 'coverflow',
+//       loop: true,
+//       speed: 1000, // ループの時間
+//       loopedSlides: 3,
+//       autoplay: {
+//         delay: 5000, // 途切れなくループ
+//       },
+//       // centeredSlides: true,
+//       // slidePerView: 1,
+//       // breakposts: {
+//       //   1024: {
+//       //     slidePerView: 2
+//       //   }
+//       // }
+//     });
+//   }
+// };
+
+
+// // direction: 'vertical',
+// // loop: true, // ループ有効
+// // slidesPerView: 3, // 一度に表示する枚数
+// // allowTouchMove: false, // スワイプ無効
