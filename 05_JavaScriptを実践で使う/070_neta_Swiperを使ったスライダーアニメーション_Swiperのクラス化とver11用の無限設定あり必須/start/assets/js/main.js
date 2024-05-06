62f2b031cb3d8ff2e9cb5d936a739a23181e5aba
Nobuyuki Takahiro <vivlio@kumihan.com>
2024-05-06 19:10:39 +0900

    create class swiper

--- 05_JavaScriptを実践で使う/070_neta_Swiperを使ったスライダーアニメーション_Swiperのクラス化とver11用の無限設定あり必須/start/assets/js/main.js
+++ 05_JavaScriptを実践で使う/070_neta_Swiperを使ったスライダーアニメーション_Swiperのクラス化とver11用の無限設定あり必須/start/assets/js/main.js
@@ -1,38 +1,52 @@
-// Ver.11は、かなりダメな仕様に成り下がっている模様。
-// かなり心配なライブラリだね。多分、またハマるわきっと。
-
-// 無限ループスライドの作成注意事項
-//    ◯CSSは『width: 100vw;』または、『width: 100%;』
-//        .swiper {
-//          width: 100vw;
-//          height: 500px;
-//          overflow: visible !important;
-//        }
-//    ◯画面に3分割のスライドを表示させたいのなら、6枚のスライドを用意。
-//    ◯ループ終端で次のループの最初が表示されない場合に『loopAdditionalSlides: 1』を設定。ただし、無くてもいい場合もある。クソだ。
-//    ◯『centeredSlides: true3』は、分割させる場合にセンターのスライドを視線誘導用にする設定。
-//    ◯『breakpoints』必須。下のコード読んだらわかる。
-
-const swiper = new Swiper('.swiper', {
-  // 多分多用するのはこちらのアニメーション。全画面でやると思う。
-  // effectがなければ画面に3枚か5枚のスライドを横に並べるものになる。
-  // effect: 'fade',
-  effect: 'coverflow',
-  loop: true,
-  // loopAdditionalSlides: 1, 
-  autoplay: {
-    delay: 2000, //=> 2秒間静止
-  },
-  speed: 1000, //=> 1秒のアニメーション
-  slidesPerView: 1,
-  breakpoints: {
-      1024: {
-          slidesPerView: 3,
-      }
-  }, 
-  centeredSlides: true,
-  // 縦向きのスライド
-  // direction: 'vertical',
-  // スワイプは、デフォルトで有効
-  // allowTouchMove: false,
+document.addEventListener('DOMContentLoaded', function() {
+  const hs = new HeroSlider('.swiper');
+  // 最小秒数を1100にしないとバグる。
+  // hs.autoStart({ delay: 1100 });
+  hs.autoStart();
+  // hs.autoStop();
 });
+
+// /////////////////////////////////////////////////////////////////
+
+// // Ver.11は、かなりダメな仕様に成り下がっている模様。
+// // かなり心配なライブラリだね。多分、またハマるわきっと。
+
+// // 無限ループスライドの作成注意事項
+// //    ◯CSSは『width: 100vw;』または、『width: 100%;』
+// //        .swiper {
+// //          width: 100vw;
+// //          height: 500px;
+// //          overflow: visible !important;
+// //        }
+// //    ◯画面に3分割のスライドを表示させたいのなら、6枚のスライドを用意。
+// //    ◯ループ終端で次のループの最初が表示されない場合に『loopAdditionalSlides: 1』を設定。ただし、無くてもいい場合もある。クソだ。
+// //    ◯『centeredSlides: true』は、分割させる場合にセンターのスライドを視線誘導用にする設定。
+// //    ◯『breakpoints』必須。下のコード読んだらわかる。
+
+// const swiper = new Swiper('.swiper', {
+//   // 多分多用するのはこちらのアニメーション。全画面でやると思う。
+//   // effectがなければ画面に3枚か5枚のスライドを横に並べるものになる。
+//   // effect: 'fade',
+//   effect: 'coverflow',
+//   loop: true,
+//   // loopAdditionalSlides: 1, 
+//   autoplay: {
+//     delay: 2000, //=> 2秒間静止
+//   // これも使用が変わっているんだね。
+//   // defaultでfalseの動きだ。
+//   // 明示的にするならtrueとして、スライドを動かしたらアニメーションが止まるに設定する。
+//   // disableOnInteraction: false
+//   },
+//   speed: 1000, //=> 1秒のアニメーション
+//   slidesPerView: 1,
+//   breakpoints: {
+//       1024: {
+//           slidesPerView: 2,
+//       }
+//   }, 
+//   centeredSlides: true,
+//   // 縦向きのスライド
+//   // direction: 'vertical',
+//   // スワイプは、デフォルトで有効
+//   // allowTouchMove: false,
+// });
